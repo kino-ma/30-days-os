882186d3331d1b29702964018758b01acee95554
kino-ma <kino.ma.ms@gmail.com>
2020-09-08 12:01:46 +0900

    change directory

--- 03_day/nasmfunc.s
+++ 03_day/nasmfunc.s
@@ -6,14 +6,6 @@
 	GLOBAL 	io_hlt
 
 ; 関数の中身
-; void io_hlt();
 io_hlt:
 	HLT
-	RET
-
-; void write_mem8(int addr, int data);
-write_mem8:
-	MOV 	ECX, [ESP + 4] 		; ESP + (sizeof int) * 1
-	MOV 	ECX, [ESP + 8] 		; ESP + (sizeof int) * 2
-	MOV 	[ECX], AL
 	RET
