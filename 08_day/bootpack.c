dfed6457ae0661647cd17be7a2f7e58eb236a02e
kino-ma <kino.ma.ms@gmail.com>
2020-09-10 21:52:11 +0900

    receive mouse values (harib05a)

--- 08_day/bootpack.c
+++ 08_day/bootpack.c
@@ -8,6 +8,7 @@ void HariMain(void) {
     extern char hankaku[4096];
     char txt[30], mcursor[256];
     unsigned char keybuf[32], mousebuf[32];
+    unsigned char mouse_dbuf[3], mouse_phase;
     int mx, my, i;
 
     init_gdtidt();
@@ -56,9 +57,25 @@ void HariMain(void) {
             }
             io_sti();
 
-            sprintf(txt, "mouse = %x", i);
-            boxfill8(binfo->vram, binfo->scrnx, 30, 50, 190, 66, DARK_MIZU);
-            draw_string(binfo->vram, binfo->scrnx, 30, 50, txt, WHITE);
+            if (mouse_phase == 0) {
+                if (i == 0xfa) {
+                    mouse_phase = 1;
+                }
+            } else if (mouse_phase == 1) {
+                mouse_dbuf[0] = i;
+                mouse_phase = 2;
+            } else if (mouse_phase == 2) {
+                mouse_dbuf[1] = i;
+                mouse_phase = 3;
+            } else if (mouse_phase == 3) {
+                mouse_dbuf[2] = i;
+                mouse_phase = 1;
+
+                sprintf(txt, "mouse = %x,%x,%x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2] );
+                boxfill8(binfo->vram, binfo->scrnx, 30, 50, 190, 66, DARK_MIZU);
+                draw_string(binfo->vram, binfo->scrnx, 30, 50, txt, WHITE);
+            }
+
         } else {
             io_stihlt();
         }
